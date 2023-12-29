f358ddd326ef7d62252601f7726eb3fa0fa8b395
Twinkle <twinklevyas047@gmail.com>
2023-12-29 14:21:30 +0530

    Added Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { useEffect, useState } from 'react'
-import { useQuery } from 'react-query'
+// import { useQuery } from 'react-query'
 
 //components
 import Item from './Item/Item'
@@ -9,6 +9,7 @@ import LinearProgress from '@material-ui/core/LinearProgress'
 import Grid from '@material-ui/core/Grid'
 // import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart'
 import Badge from '@material-ui/core/Badge'
+import NavBar from './Item/NavBar'
 
 
 //styles
@@ -39,21 +40,39 @@ export type CartItemType = {
 const App = () => {
   const [cartOpen, setCartOpen] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
   // const [cartItems, setCartItems] = useState([] as CartItemType[])
   const [data, setData] = useState([] as CartItemType[])
 
-  const getProducts = async () => {
-    setIsLoading(true)
-    const productsArr = await apiCall.get("products") // use axios(instance) instead of this 
-    setData(productsArr.data)
-    setIsLoading(false)
-  }
 
   
 
+  const getProducts = async () => {
+    try {
+      setIsLoading(true);
+      const productsArr = await apiCall.get("products"); // use axios(instance) instead of this 
+      setData(productsArr.data);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      setError(true)
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+
+
   useEffect(() => {
     getProducts();
-  },[]);
+
+  }, []);
+
+  useEffect(() => {
+    data.map((d)=> console.log(d.category))
+    
+  }, [data]);
+
+
 
   const getTotalItems = () => null;
 
@@ -62,25 +81,31 @@ const App = () => {
   // const handleRemoveFromCart = () => null;
 
   if (isLoading) return <LinearProgress />
-  // if (error) return <div>Something went wrong ...</div>
+  if (error) return <div>Something went wrong ...</div>
 
+  
+  
 
   return (
-    <Wrapper>
-      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
-        Cart goes here
-      </Drawer>
-      <StyledButton onClick={() => setCartOpen(true)}>
-        <Badge badgeContent={getTotalItems()}></Badge>
-      </StyledButton>
-      <Grid container spacing={3}>
-        {data?.map(item => (
-          <Grid item key={item.id} xs={12} sm={3}>
-            <Item item={item} handleAddToCart={handleAddToCart}></Item>
-          </Grid>
-        ))}
-      </Grid>
-    </Wrapper>
+    <>
+      <NavBar />
+      <Wrapper>
+
+        <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+          Cart goes here
+        </Drawer>
+        <StyledButton onClick={() => setCartOpen(true)}>
+          <Badge badgeContent={getTotalItems()}></Badge>
+        </StyledButton>
+        <Grid container spacing={3}>
+          {data?.map(item => (
+            <Grid item key={item.id} xs={12} sm={3}>
+              <Item item={item} handleAddToCart={handleAddToCart}></Item>
+            </Grid>
+          ))}
+        </Grid>
+      </Wrapper>
+    </>
   );
 }
 
